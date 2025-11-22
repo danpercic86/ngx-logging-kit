708958e612c909da66f3518333a31c107456cc45
Dan Percic <danpercic86@gmail.com>
2025-11-22 18:56:45 +0200

    ng 15

--- projects/demo/src/app/log-config/log-config.component.ts
+++ projects/demo/src/app/log-config/log-config.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import { MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { MatLegacySlideToggleChange as MatSlideToggleChange } from '@angular/material/legacy-slide-toggle';
 import { NgxLoggerLevel } from '../../../../../src/public_api';
 
 @Component({
